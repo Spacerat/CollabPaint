1afdc613f4e8b6d47166199128087aa72ae10920
Spacerat <spacerat3004@gmail.com>
2011-05-15 09:53:17 +0100

    Fixed an error with members on room delete + test stuff.

--- paintserver.js
+++ paintserver.js
@@ -58,13 +58,15 @@ var Room = function(url, rooms) {
     var members = [];
  	var doc = new paint.Document();   
     var max_members = 255;
-    var timeout_time = 1000 * 60 * 60; 
+    var timeout_time = 1000 * 60; 
     var timeout;
     var images = {};
     var timeout_func = function() {
-    	for (var c in members) {
-    		members[c].Disconnect("The room has timed out.");
-    	}
+    	console.log("Timeout");
+    	members.forEach(function(c) {
+    		console.log(c);
+    		c.Disconnect("The room has timed out.");
+    	});
     	//Delete room
     	delete rooms[url];
     	members = [];
